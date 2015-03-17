d9c773381163d4d01a0e73de11c289e8e3b5f4bd
Thomas Boerger <tboerger@suse.de>
2015-03-17 11:45:57 +0100

    Refresh the repos after added

--- recipes/default.rb
+++ recipes/default.rb
@@ -26,7 +26,7 @@ when "suse"
     key node["xca"]["zypper"]["key"]
     title node["xca"]["zypper"]["title"]
 
-    action :add
+    action [:add, :refresh]
 
     only_if do
       node["xca"]["zypper"]["enabled"]
